3658ac603f5ee4bd1c9f14812d97da64a6848a67
takaya-47 <local.s.s.47@gmail.com>
2026-01-27 07:09:45 +0000

    stringsパッケージを使ってみる

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,15 @@
 package iteration
 
+import "strings"
+
 func Repeat(character string, times int) string {
 	var repeated string
 	for range times {
 		repeated += character
 	}
 	return repeated
+}
+
+func ToLower(char string) string {
+	return strings.ToLower(char)
 }
