0882d22f56fe1ed81de0a914ab1e960420f15efc
takaya-47 <local.s.s.47@gmail.com>
2026-01-28 06:19:39 +0000

    章の終わり

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -8,12 +8,16 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+			continue
+		}
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
 	}
 
 	return sums
