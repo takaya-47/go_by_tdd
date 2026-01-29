5fda0da1f825166d07eaf3a28cc574be97eaf453
takaya-47 <local.s.s.47@gmail.com>
2026-01-29 07:25:43 +0000

    残高不足を考慮

--- pointersAndErrors/pointer_test.go
+++ pointersAndErrors/pointer_test.go
@@ -12,6 +12,14 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t *testing.T, err error) {
+		t.Helper()
+
+		if err == nil {
+			t.Error("wanted an error, but didn't get one")
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10)) // Bitcoin型にキャスト
@@ -24,5 +32,15 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10)) // Bitcoin型にキャスト
 
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		initialBalance := Bitcoin(20)
+		wallet := Wallet{balance: initialBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, initialBalance)
+
+		assertError(t, err)
 	})
 }
