9de1283da634fed81cf56538f0b9d6e35dd029dc
takaya-47 <local.s.s.47@gmail.com>
2026-01-29 06:48:52 +0000

    テストをリファクタ

--- pointersAndErrors/pointer_test.go
+++ pointersAndErrors/pointer_test.go
@@ -3,27 +3,26 @@ package pointersanderrors
 import "testing"
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10)) // Bitcoin型にキャスト
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got  != want {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10)) // Bitcoin型にキャスト
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got  != want {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }
