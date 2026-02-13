8ec509c8c42c31733887830cfa18d9d476b83d82
takaya-47 <local.s.s.47@gmail.com>
2026-02-13 05:24:50 +0000

    一旦目標まで実装

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func CountDown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -11,7 +11,10 @@ func TestCountD(t *testing.T) {
 	CountDown(buffer)
 
 	got := buffer.String()
-	want := "3"
+	want := `3
+2
+1
+Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
