caa06cf28e7822c4ba2caa13e5f79997f4431f46
takaya-47 <local.s.s.47@gmail.com>
2026-02-18 07:53:00 +0000

    selectで実装

--- selection/selection.go
+++ selection/selection.go
@@ -2,21 +2,27 @@ package selection
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
+	// 先にレスポンスが帰ってきたURLを返却すればいいのでselectを使う
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+
+	// 監視対象のチャネルを返す前にゴルーチンを予約。
+	go func() {
+		http.Get(url)
+		close(ch) // Racer側でどれか一つのチャネルが閉じられたらRacer自体の処理が終了してしまうので、ping側では常にチャネルを閉じる必要がある。チャネルに値を送信する処理としてしまうと、Racer側で処理が終了した後に受信する相手がいなくなってしまい、残っとゴルーチンがゾンビ化する。
+	}()
+
+	// 一旦チャネルを返し、selectで監視に入る。あとはゴルーチンの処理が完了してチャネルが閉じられるのを待つだけ。
+	return ch
 }
