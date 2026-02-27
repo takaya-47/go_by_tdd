842207d1dc1f241a37447cec437e3907d3f0e83c
takaya-47 <local.s.s.47@gmail.com>
2026-02-27 02:12:48 +0000

    walk関数を修正し、全てのフィールドを処理するように変更。テストケースを整理し、期待される呼び出しを確認するように実装。

--- reflection/reflect.go
+++ reflection/reflect.go
@@ -1,14 +1,13 @@
 package reflection
 
 import (
-	// "fmt"
 	"reflect"
 )
 
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
-// fmt.Printf("Type: %v, Kind: %v, Value: %v\n", val.Type(), val.Kind(), val)
-	field := val.Field(0)
-// fmt.Printf("Type: %v, Kind: %v, Value: %v\n", field.Type(), field.Kind(), field)
-	fn(field.String())
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
 }
