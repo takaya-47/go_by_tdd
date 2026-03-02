b8327965237dacfb9318d6e0206454f5c77d8780
takaya-47 <local.s.s.47@gmail.com>
2026-03-02 00:46:38 +0000

    TestWalk関数にネストされたフィールドのテストケースを追加

--- reflection/reflect_test.go
+++ reflection/reflect_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name string
+	Profile Profile
+}
+
+type Profile struct {
+	Age int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 	cases := []struct{
 		Name string
@@ -24,6 +34,11 @@ func TestWalk(t *testing.T) {
 				Name string
 				City string
 			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Nested fields",
+			Person{"Chris", Profile{33, "London"}},
 			[]string{"Chris", "London"},
 		},
 	}
