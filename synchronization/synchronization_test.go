a2ee19f84229124e46d42a5406b66bf8835ab9ee
takaya-47 <local.s.s.47@gmail.com>
2026-03-03 03:32:51 +0000

    TestCounter関数の実装を修正し、Counter構造体のインスタンスをNewCounter()で生成するように変更。また、assertCounter関数の引数をポインタ型に修正し、テストの整合性を向上。

--- synchronization/synchronization_test.go
+++ synchronization/synchronization_test.go
@@ -1,19 +1,40 @@
 package synchronization
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func (t *testing.T)  {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
 	})
+
+	t.Run("it runs safely in concurrently", func (t *testing.T)  {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				defer w.Done()
+
+				counter.Inc()
+			}(&wg)
+		}
+		wg.Wait() // ここで全てのゴルーチン実行を待つ（w.Done()を待つ。）
+		assertCounter(t, counter, wantedCount)
+	})
 }
 
-func assertCounter(t *testing.T, got Counter, want int) {
+func assertCounter(t *testing.T, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), want)
