553b4785d16173e2711046e4b441928c3c21dbe6
takaya-47 <local.s.s.47@gmail.com>
2026-03-04 07:39:56 +0000

    commit

--- propertyBasedTests/property.go
+++ propertyBasedTests/property.go
@@ -2,36 +2,87 @@ package propertybasedtests
 
 import "strings"
 
-type RomanNumeral struct {
-	Value  int
-	Symbol string
+func ConvertToArabic(roman string) (total uint16) {
+    for _, symbols := range windowedRoman(roman).Symbols() {
+        total += uint16(allRomanNumerals.ValueOf(symbols...))
+    }
+    return
 }
 
-var allRomanNumerals = []RomanNumeral{
-	{1000, "M"},
-	{900, "CM"},
-	{500, "D"},
-	{400, "CD"},
-	{100, "C"},
-	{90, "XC"},
-	{50, "L"},
-	{40, "XL"},
-	{10, "X"},
-	{9, "IX"},
-	{5, "V"},
-	{4, "IV"},
-	{1, "I"},
+func ConvertToRoman(arabic uint16) string {
+    var result strings.Builder
+
+    for _, numeral := range allRomanNumerals {
+        for arabic >= uint16(numeral.Value) {
+            result.WriteString(numeral.Symbol)
+            arabic -= uint16(numeral.Value)
+        }
+    }
+
+    return result.String()
 }
 
-func ConvertToRoman(arabic int) string {
-	var result strings.Builder
+type romanNumeral struct {
+    Value  int
+    Symbol string
+}
 
-	for _, numeral := range allRomanNumerals {
-		for arabic >= numeral.Value {
-			result.WriteString(numeral.Symbol)
-			arabic -= numeral.Value
-		}
-	}
+type romanNumerals []romanNumeral
 
-	return result.String()
+func (r romanNumerals) ValueOf(symbols ...byte) int {
+    symbol := string(symbols)
+    for _, s := range r {
+        if s.Symbol == symbol {
+            return s.Value
+        }
+    }
+
+    return 0
+}
+
+func (r romanNumerals) Exists(symbols ...byte) bool {
+    symbol := string(symbols)
+    for _, s := range r {
+        if s.Symbol == symbol {
+            return true
+        }
+    }
+    return false
+}
+
+var allRomanNumerals = romanNumerals{
+    {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
+    {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"},
+}
+
+type windowedRoman string
+
+func (w windowedRoman) Symbols() (symbols [][]byte) {
+    for i := 0; i < len(w); i++ {
+        symbol := w[i]
+        notAtEnd := i+1 < len(w)
+
+        if notAtEnd && isSubtractive(symbol) && allRomanNumerals.Exists(symbol, w[i+1]) {
+            symbols = append(symbols, []byte{byte(symbol), byte(w[i+1])})
+            i++
+        } else {
+            symbols = append(symbols, []byte{byte(symbol)})
+        }
+    }
+    return
+}
+
+func isSubtractive(symbol uint8) bool {
+    return symbol == 'I' || symbol == 'X' || symbol == 'C'
 }
--- propertyBasedTests/property_test.go
+++ propertyBasedTests/property_test.go
@@ -2,10 +2,12 @@ package propertybasedtests
 
 import (
 	"fmt"
+	"log"
 	"testing"
+	"testing/quick"
 )
 
-var cases = []struct{
+var cases = []struct {
 	Arabic int
 	Roman  string
 }{
@@ -53,13 +55,31 @@ func TestConvertingToRoman(t *testing.T) {
 
 }
 func TestConvertingToArabic(t *testing.T) {
-	for _, test := range cases[:1] {
-		t.Run(fmt.Sprintf("%d gets converted to %q", test.Roman, test.Arabic), func(t *testing.T) {
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
 			got := ConvertToArabic(test.Roman)
 
 			if got != test.Arabic {
 				t.Errorf("got %d, want %d", got, test.Arabic)
 			}
 		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, &quick.Config{
+		MaxCount: 1000,
+	}); err != nil {
+		t.Error("failed checks", err)
 	}
 }
