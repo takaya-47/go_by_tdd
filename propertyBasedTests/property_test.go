5728e64d16b8632f0e10dcd82b0a419de9938757
takaya-47 <local.s.s.47@gmail.com>
2026-03-04 06:05:04 +0000

    テストケースを追加し、ローマ数字変換のテストを改善

--- propertyBasedTests/property_test.go
+++ propertyBasedTests/property_test.go
@@ -3,10 +3,21 @@ package propertybasedtests
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	} {
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+	}
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	for _, test := range cases {
+		got := ConvertToRoman(test.Arabic)
+
+		if got != test.Want {
+			t.Errorf("got %q, want %q", got, test.Want)
+		}
 	}
 }
