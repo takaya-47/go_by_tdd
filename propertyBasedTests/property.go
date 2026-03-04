67e880ff581f2d51a4e27233cc7ad198fcc52e7f
takaya-47 <local.s.s.47@gmail.com>
2026-03-04 06:05:11 +0000

    ローマ数字変換関数を修正し、アラビア数字に基づいて「I」を繰り返し生成するように変更

--- propertyBasedTests/property.go
+++ propertyBasedTests/property.go
@@ -1,5 +1,13 @@
 package propertybasedtests
 
+import "strings"
+
 func ConvertToRoman(arabic int) string {
-	return "I"
+	var result strings.Builder
+
+	for i := 0; i< arabic; i++ {
+		result.WriteString("I")
+	}
+
+	return result.String()
 }
